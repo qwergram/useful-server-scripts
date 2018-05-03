54baea7c151404a477174f8de0e469b1a2d87ccd
Norton Pengra <npengra317@gmail.com>
2018-05-03 11:46:27 -0700

    Create login-loop-fix.sh

---
+++ login-loop-fix.sh
@@ -0,0 +1,5 @@
+# https://askubuntu.com/questions/223501/ubuntu-gets-stuck-in-a-login-loop
+# Use CTRL+ALT+F3
+cd /home/qwergram/
+chown qwergram:qwergram .Xauthority
+chown qwergram:qwergram .ICEauthority
